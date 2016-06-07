646c3960d11e5dd6a6a6b6ffe297fd49660a95f9
a-murphy <a-murphy@users.noreply.github.com>
2016-06-07 13:24:24 -0700

    Update test.js

--- test.js
+++ test.js
@@ -38,10 +38,10 @@ describe('Postgres Database', function () {
       if (err)
         return console.error('could not connect to postgres', err);
 
-      client.query('SELECT NOW() AS "theTime"', function (err, result) {
+      client.query('SELECT * from things;', function (err, result) {
         if (err)
           return console.error('error running query', err);
-        console.log(result.rows[0].theTime);
+        expect(docs[0].name).to.equal('Johnson');
         client.end();
         done();
       });
